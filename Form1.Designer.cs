ea626335bfc27d51aa26c1d60becfc6e55c34472
Ilyshin <90715587+Ilyshin@users.noreply.github.com>
2021-11-14 17:55:19 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findContoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findSquareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1468, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findContoursToolStripMenuItem});
+            this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // findContoursToolStripMenuItem
+            // 
+            this.findContoursToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findSquareToolStripMenuItem});
+            this.findContoursToolStripMenuItem.Name = "findContoursToolStripMenuItem";
+            this.findContoursToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.findContoursToolStripMenuItem.Text = "Find_Contours";
+            this.findContoursToolStripMenuItem.Click += new System.EventHandler(this.findContoursToolStripMenuItem_Click);
+            // 
+            // findSquareToolStripMenuItem
+            // 
+            this.findSquareToolStripMenuItem.Name = "findSquareToolStripMenuItem";
+            this.findSquareToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(60, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 613);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(608, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(506, 613);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PNG|*.png";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(1148, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 29);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1148, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(1468, 799);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findContoursToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem findSquareToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Microsoft.VisualBasic;
+using ZedGraph;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> inputIm = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult res = openFileDialog1.ShowDialog();
+            inputIm = new Image<Bgr, byte>(openFileDialog1.FileName);
+            pictureBox1.Image = inputIm.Bitmap;
+        }
+
+        private void findContoursToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double f = double.Parse(Interaction.InputBox("Введите масштаб", "Поиск площади"));
+            int l = int.Parse(Interaction.InputBox("Введите число бросаний", "Поиск площади"));
+            Image<Gray, byte> img = inputIm.SmoothGaussian(7).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(50), new Gray(255)); //Значения площади могут заметно отличаться в зависимости от аргумента SmoothGaussian()
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat hieratchy = new Mat();
+            Image<Gray, byte> blackBackground = new Image<Gray, byte>(inputIm.Width, inputIm.Height, new Gray(0));
+            CvInvoke.FindContours(img, contours, hieratchy, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);
+            int n = FindLake(blackBackground, contours, l);
+            pictureBox2.Image = blackBackground.Bitmap;
+            var s = GetS(blackBackground, l, f, contours, n);
+            CvInvoke.DrawContours(blackBackground, contours, n , new MCvScalar(255, 0, 0));
+            pictureBox2.Image = blackBackground.Bitmap;
+            textBox1.Text = s.s.ToString() + "\t" + s.sigma.ToString();
+        }
+
+        private static int FindLake (Image<Gray, byte> blackBackground, VectorOfVectorOfPoint contours, int l)
+        {
+            Random rnd = new Random();
+            var countOfPointsInContour = new double[contours.Size];
+            for (int i = 0; i < l / 2; i++)
+            {
+                var point = new PointF(rnd.Next(blackBackground.Width), rnd.Next(blackBackground.Height));
+                for (int j = 0; j < contours.Size; j++)
+                {
+                    var a = CvInvoke.PointPolygonTest(contours[j], new PointF(rnd.Next(blackBackground.Width), rnd.Next(blackBackground.Height)), false);
+                    if (a >= 0)
+                        countOfPointsInContour[j] += 1;
+                }
+            }
+            var count = double.MinValue;
+            int index = 0;
+            for (int i = 0; i<countOfPointsInContour.Length; i++)
+            {
+                if (countOfPointsInContour[i] > count)
+                {
+                    count = countOfPointsInContour[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+        private static (double s, double sigma) GetS (Image<Gray, byte> blackBackground, double l, double f, VectorOfVectorOfPoint contours, int n)
+        {
+            Random rnd = new Random();
+            int m = 10;
+            var s = new double[m];
+            for (int j = 0; j < m; j++)
+            {
+                int k = 0;
+                for (int i = 0; i < l; i++)
+                {
+                    var a = CvInvoke.PointPolygonTest(contours[n], new PointF(rnd.Next(blackBackground.Width), rnd.Next(blackBackground.Height)), false);
+                    if (a > 0)
+                        k += 1;
+                }
+                s[j] = k / l * f;
+            }
+            double avarage = 0;
+            double sqAvarage = 0;
+            for (int i = 0; i < m; i++)
+            {
+                avarage += s[i] / m;
+                sqAvarage += s[i] * s[i] / m;
+            }
+            double sigmaM = Math.Sqrt((sqAvarage - avarage * avarage));
+            var sTurple = (Math.Round(avarage,3), sigmaM);
+            return sTurple;
+        }
+
+        
+    }
+}
